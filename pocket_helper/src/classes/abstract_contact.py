3f34d1ef6f14180a214329718e6990f70290a693
ValeraPrus <146940361+ValeraPrus@users.noreply.github.com>
2024-02-11 17:53:33 +0200

    Delete pocket_helper directory

--- pocket_helper/src/classes/abstract_contact.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ContactData(ABC):
-
-    @abstractmethod
-    def contact_data_output(self):
-        pass
--- pocket_helper/src/classes/abstract_notes.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class NotesData(ABC):
-
-    @abstractmethod
-    def notes_data_output(self):
-        pass
